e8d74465705e2d8d31383f356534fe8c2397d267
lnsaneboy <1764914125@qq.com>
2020-12-05 20:04:50 +0800

    用函数完善二分法

---
+++ erfenfa/erfenfa.c
@@ -0,0 +1,40 @@
+//用函数实现二分法
+#include <stdio.h>
+int binary_search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr, k, sz);
+	if (ret == -1)
+	{
+		printf("找不到指定数字\n");
+	}
+	else
+	{
+		printf("找到了，下标是:%d\n", ret);
+	}
+	return 0;
+}
