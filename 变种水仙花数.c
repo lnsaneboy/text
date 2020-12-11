756777a3a84cbc02bda0361c44594bd05714450d
lnsaneboy <1764914125@qq.com>
2020-12-11 18:30:44 +0800

    求出5位数中的所有的Lily Number

---
+++ 变种水仙花数.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    for(i=10000;i<=99999;i++)
+    {
+        int j=0;
+        int sum=0;
+        for(j=10;j<=10000;j*=10)
+        {
+            sum+=((i/j)*(i%j));
+        }
+        if(sum==i)
+            printf("%d ",i);
+    }
+    return 0;
+}
